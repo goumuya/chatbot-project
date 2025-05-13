ab06ed899748bc8c2adfaa8ea2667464cdd2a587
KimHyunSoo <goumuya@naver.com>
2025-05-13 09:59:37 +0900

    API키 수정

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ load_dotenv()
 
 client = OpenAI(
     #api_key=os.getenv("GROQ_API_KEY"),
-    api_key=os.getenv("OPENAI_PAID_API_KEY"),
+    #api_key=os.getenv("OPENAI_PAID_API_KEY"),
+    api_key=st.secrets["OPENAI_PAID_API_KEY"],
     #base_url="https://api.groq.com/openai/v1"
 )
 
